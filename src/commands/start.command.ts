cf9226216f8d20c88ba0eb3b2a329337019325f6
bazaq <basarus51@gmail.com>
2023-06-23 19:56:43 +0300

    upd

---
+++ src/commands/start.command.ts
@@ -0,0 +1,29 @@
+import { IBotContext } from "../context/context.interface";
+import { Command } from "./command.class";
+import { Markup, Telegraf } from 'telegraf'
+
+export class StartCommand extends Command {
+    constructor(bot: Telegraf<IBotContext>) {
+        super(bot)
+    }
+
+    handle(): void {
+        this.bot.start((ctx) => {
+            console.log(ctx.session);
+            ctx.reply("Вам понравился курс?", Markup.inlineKeyboard([
+                Markup.button.callback("Like", "course_like"),
+                Markup.button.callback("Dislike", "course_dislike"),
+            ]))
+        })
+
+        this.bot.action("course_like", (ctx) => {
+            ctx.session.courseLike = true;
+            ctx.editMessageText("Круто!")
+        })
+
+        this.bot.action("course_dislike", (ctx) => {
+            ctx.session.courseLike = false;
+            ctx.editMessageText("Как же так!")
+        })
+    }
+}
---
+++ src/config/config.service.ts
@@ -0,0 +1,21 @@
+import { config, DotenvParseOutput } from "dotenv";
+import { IConfigService } from "./config.interface";
+
+export class ConfigService implements IConfigService {
+
+    private config: DotenvParseOutput;
+
+    constructor() {
+        const { error, parsed } = config();
+        if (error) throw Error('Не найден файл .env');
+        if (!parsed) throw Error('Пустой .env');
+
+        this.config = parsed;
+    }
+
+    get(key: string): string {
+        const res = this.config[key];
+        if (!res) throw Error('Нет такого ключа');
+        return res;
+    }
+}
